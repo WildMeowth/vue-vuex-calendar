bef15ac9c185ec71c13c149affca4ffbe8f12364
Colin Miao <sddzok@163.com>
2017-11-06 10:02:53 +0800

    modify store

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,16 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import mutations from './mutations';
 import actions from './actions';
-
+import Util from '../util/util.js'
 Vue.use(Vuex);
 
+const userName = Util.getCookie('name');
 const state = {
   totalTime: parseInt(6),
   list: [
     {
-      name: 'colin',
-      avatar: require('../assets/Colin.jpg'),
+      name: userName,
+      avatar: require('../assets/colin.jpg'),
       date: '2017-10-12',
       totalTime: parseInt(6),
       comment: '10月12日晚上，看qqq直播6小时'
